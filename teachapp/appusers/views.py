4b52ef4f0d1e044e28d96f444f9ff00bb3de7e4b
Karmanya Tyagi <karmanya100@gmail.com>
2021-08-29 01:45:01 +0530

    motorq message

---
+++ teachapp/appusers/views.py
@@ -0,0 +1,95 @@
+from django.urls import reverse
+from django.contrib.auth import authenticate, login,logout
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from appusers.forms import UserForm,UserProfileInfoForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from curriculum.models import Standard
+from .models import user_profile, Contact
+from django.views.generic import CreateView
+
+
+def index(request):
+    return render(request,'base.html')
+
+
+
+def register(request):
+    registered = False
+
+    if request.method =="POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.erros,profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+
+    return render(request,'appusers/registration.html',{'registered':registered,'user_form':user_form,'profile_form':profile_form})
+
+
+def user_login(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        user = authenticate(username=username,password=password)
+
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("Account Is Deactivated")
+        else:
+            return HttpResponse("Please use correct id and password")
+
+
+    else:
+        return render(request,'appusers/login.html')
+
+
+@login_required
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+
+class HomeView(TemplateView):
+    template_name = 'appusers/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        standards = Standard.objects.all()
+        teachers = user_profile.objects.filter(user_type='teacher')
+        context['standards'] = standards
+        context['teachers'] = teachers
+        return context
+
+class ContactView(CreateView):
+    model = Contact
+    fields = '__all__'
+    template_name = 'appusers/contact.html'
